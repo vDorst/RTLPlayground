72d8f3639c8f4822571d7fcfd197df2bde501a8e
logicog <git@logicog.de>
2025-08-19 09:26:48 +0200

    Script to generate port overview dynamically

---
+++ html/ports.js
@@ -0,0 +1,22 @@
+function drawPorts() {
+  var f = document.getElementById('ports');
+  console.log("DRAWING PORTS: ", numPorts);
+  for (let i = 0; i < numPorts; i++) {
+    console.log("DRAWING isSFP: ", pIsSFP[i]);
+    const l = document.createElement("object");
+    l.type = "image/svg+xml";
+    if (!pIsSFP[i]) {
+      l.data = "port.svg";
+      l.width ="40";
+      l.height ="40";
+    } else {
+      l.data = "sfp.svg";
+      l.width = "60";
+      l.height = "60";
+    }
+    l.id="port" + (i+1);
+    f.appendChild(l);
+  }
+  
+  console.log("DRAWING DONE ");
+}

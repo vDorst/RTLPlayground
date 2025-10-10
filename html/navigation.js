09ba7859b61ba0e0980d79fead2e93d4fb6cbccc
logicog <git@logicog.de>
2025-10-10 18:27:24 +0200

    Add navigation.js sidebar

---
+++ html/navigation.js
@@ -0,0 +1,9 @@
+document.getElementById('sidebar').innerHTML =
+ "<ul><li><a href='index.html'>Overview</a></li>"
+ + "<li><a href='stat.html'>Port Statistics</a></li>"
+ + "<li><a href='vlan.html'>VLAN</a></li>"
+ + "<li><a href='mirror.html'>Mirroring</a></li>"
+ + "<li><a href='trunk.html'>Port Aggregation</a></li>"
+ + "<li><a href='mirror.html'>Mirroring</a></li>"
+ + "<li><a href='update.html'>Firmware Update</a></li></ul>";
+ 

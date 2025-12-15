87717a0790f930102bfcf4ffe412ebb232eee418
logicog <git@logicog.de>
2025-12-15 08:08:00 +0100

    Move drawing of ports into main.js
    This reduces the number of .js which are pulled by the browser
    when loading a http page, which should reduce the loading time.
    Also prepare for device-dependent logical to physical port
    mappings.

--- html/main.js
+++ html/main.js
@@ -6,6 +6,30 @@ const linkS = ["Disabled", "No Link", "100M", "1000M", "500M", "10G", "2.5G", "5
 var pState = new Int8Array(10);
 var pIsSFP = new Int8Array(10);
 var numPorts = 0;
+const logToPhysPort = [0, 0, 0, 5, 1, 2, 3, 4, 6];
+const physToLogPort = [	4, 5, 6, 7, 3, 8];
+
+function drawPorts() {
+  var f = document.getElementById('ports');
+  console.log("DRAWING PORTS: ", numPorts);
+  for (let i = 0; i < numPorts; i++) {
+    console.log("DRAWING isSFP: ", pIsSFP[i]);
+    const l = document.createElement("object");
+    l.type = "image/svg+xml";
+    if (!pIsSFP[i]) {
+      l.data = "port.svg";
+      l.width ="40";
+      l.height ="40";
+    } else {
+      l.data = "sfp.svg";
+      l.width = "60";
+      l.height = "60";
+    }
+    l.id="port" + (i+1);
+    f.appendChild(l);
+  }
+  console.log("DRAWING DONE ");
+}
 
 function update() {
   var xhttp = new XMLHttpRequest();

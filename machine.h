9e911dd23fdc126794185d245dda14ca993aca1a
René van Dorst <opensource@vdorst.com>
2025-12-21 11:09:21 +0100

    KP-9000-9XHML-X: Add basic support

--- machine.h
+++ machine.h
@@ -6,10 +6,11 @@
 /*
  * Select your machine type below
  */
-#define MACHINE_KP_9000_6XHML_X2
+//#define MACHINE_KP_9000_6XHML_X2
+#define MACHINE_KP_9000_9XHML_X /* See doc/devices/2M-PCB23-V3_1.md */
 // #define MACHINE_KP_9000_6XH_X
 // #define MACHINE_KP_9000_9XH_X_EU
-// #define MACHINE_SWGT024_V2_0
+// #define MACHINE_SWGT024_V2_0 /* See doc/devices/SWTG024AS.md */
 // #define MACHINE_HORACO_ZX_SG4T2
 
 // #define DEFAULT_8C_1SFP

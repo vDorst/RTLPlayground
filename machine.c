1557a7cf3ce0b932e0da489cc12c261b1c62b26e
René van Dorst <opensource@vdorst.com>
2026-09-20 11:30:17 +0200

    MACHINE: rebase, update the new added devices

--- machine.c
+++ machine.c
@@ -1179,10 +1179,7 @@ __code const struct machine machine = {
     .sfp_port[0].pin_los = GPIO37,
     .sfp_port[0].pin_tx_disable = GPIO_NA,
     .sfp_port[0].sds = 1,
-    .sfp_port[0].i2c = {
-        .sda = GPIO39_I2C_SDA4,
-        .scl = GPIO40_I2C_SCL3_MDC1
-    },
+    .sfp_port[0].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
 
     .reset_pin = GPIO_NA,
 	.high_leds = { .mux = 0, .enable = 0 },
@@ -1216,7 +1213,7 @@ __code const struct machine machine = {
 	.sfp_port[0].pin_los = GPIO37,
 	.sfp_port[0].pin_tx_disable = GPIO_NA,
 	.sfp_port[0].sds = 1,
-	.sfp_port[0].i2c = { .sda = GPIO39_I2C_SDA4, .scl = GPIO40_I2C_SCL3_MDC1 },
+	.sfp_port[0].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
 	.reset_pin = GPIO_NA,
 	.high_leds = { .mux = LED_27 | LED_29, .enable = LED_28_SYS | LED_29 },
 	.port_led_set = { 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -1260,7 +1257,7 @@ __code const struct machine machine = {
 	.sfp_port[0].pin_los = GPIO37,
 	.sfp_port[0].pin_tx_disable = GPIO_NA,
 	.sfp_port[0].sds = 1,
-	.sfp_port[0].i2c = { .sda = GPIO39_I2C_SDA4, .scl = GPIO40_I2C_SCL3_MDC1 },
+	.sfp_port[0].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ),
 
 	.reset_pin = GPIO_NA,
 
